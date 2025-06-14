bd92c3c7b93c534ed6696871f92b63bd97308da8
AlekseyF3 <56193692+AlekseyF3@users.noreply.github.com>
2025-06-14 14:32:03 +0300

    Update task1.py

--- Лабораторная работа 3/task1.py
+++ Лабораторная работа 3/task1.py
@@ -17,7 +17,10 @@ class Book:
         return f"Книга {self.name}. Автор {self.author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{
+            self.__class__.__name__}(name={
+            self.name!r}, author={
+            self.author!r})"
 
 
 class PaperBook(Book):
